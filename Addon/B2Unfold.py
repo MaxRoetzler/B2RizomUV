daad240e2d021496b06f67c96d2d36499320d26d
Erik Sutton <Eriks.indesign@gmail.com>
2017-11-06 22:07:00 +0100

    broken

--- Addon/B2Unfold.py
+++ Addon/B2Unfold.py
@@ -18,11 +18,14 @@ import sys
 import tempfile
 from sys import platform
 
-print(platform)
+print(str(bpy.context.scene.B2Unfold_Settings.sharpestAngle))
 
 def B2Unfold_LinkFunction():
     
-    path = "" + tempfile.gettempdir()
+    if platform == "darwin":
+        path = "" + tempfile.gettempdir()
+    elif platform == "win32":
+        path = "" + bpy.app.tempdir   
     path = '/'.join(path.split('\\'))
     objName = "Tmp.obj"
     originalObj = bpy.data.objects.get(bpy.context.active_object.name)
@@ -39,8 +42,7 @@ def B2Unfold_LinkFunction():
     print(cmdModel_string)
 
     cmd1_string = "U3dIslandGroups({Mode='SetGroupsProperties', MergingPolicy=8322, GroupPaths={ 'RootGroup' }, Properties={Pack={Resolution=2000}}})\n\
-    U3dSelect({PrimType='Edge', Select=true, All=true, FilterIslandVisible=true})\n\
-    U3dSelect({PrimType='Edge', Select=true, ResetBefore=true, WorkingSetPrimType='Island', ProtectMapName='Protect', FilterIslandVisible=true, Auto={QuasiDevelopable={Developability=0.95, IslandPolyNBMin=5, FitCones=false, Straighten=true}, PipesCutter=true, HandleCutter=true, StretchLimiter=true, SkeletonUnoverlap={SegLevel=1, FromRoot=true, Smooth=2}, SQS=0.0357143, SQP=0.5, AQS=0.000178571, AQP=0.5, Smooth={Iterations=2, Force=0.5}}})\n\
+    " + algorithmString + "\n\
     U3dCut({PrimType='Edge'})\n\
     U3dUnfold({PrimType='Edge', MinAngle=1e-005, Mix=1, Iterations=250, PreIterations=5, StopIfOutOFDomain=false, RoomSpace=0, PinMapName='Pin', ProcessNonFlats=true, ProcessSelection=true, ProcessAllIfNoneSelected=true, ProcessJustCut=true, BorderIntersections=true, TriangleFlips=true})\n\
     U3dIslandGroups({Mode='DistributeInTilesEvenly', MergingPolicy=8322, GroupPath='RootGroup'})\n\
@@ -48,8 +50,7 @@ def B2Unfold_LinkFunction():
     U3dIslandGroups({Mode='DistributeInTilesByBBox', MergingPolicy=8322})\n\
     U3dIslandGroups({Mode='DistributeInTilesEvenly', MergingPolicy=8322, UseTileLocks=true, UseIslandLocks=true})\n\
     U3dPack({ProcessTileSelection=false, RecursionDepth=1, RootGroup='RootGroup', Scaling={Mode=0}, Rotate={Mode=0}, Translate=true, LayoutScalingMode=2})\n\
-    U3dSave({File={Path='" + path + objName + "', UVWProps=true}, __UpdateUIObjFileName=true})\n\
-    U3dQuit()" 
+    U3dSave({File={Path='" + path + objName + "', UVWProps=true}, __UpdateUIObjFileName=true})\n" 
 
 
     f = open(path + "Unwrap.lua", "w") 
@@ -93,19 +94,38 @@ def get_unfold3DPath(self):
     except:
         return ""
 
+def jangle(self, context):
+    print("update called on scene ", self.name)
+    # do something based on the value of "my_prop"
+    if self.my_prop:
+        print("It's on")
+    else:
+        print("It's off")
+    return None
+
+def set_algorithm(self, context):
+    global algorithmCMDStrings
+    global algorithmString
+    algorithmString = algorithmCMDStrings[(int(self.autoSeamAlgorithm))]
+    print(algorithmString)
+
+def set_mosaicForce(value):
+    
+    bpy.context.scene.B2Unfold_Settings.mosaicForce = value
+
 # ------------------------------------------- SETTINGS -----------------------------------------------
 
 class B2Unfold_Settings(bpy.types.PropertyGroup):
     unfold3DPath = bpy.props.StringProperty \
     (
         name = "",
-        description = "Set the path to the Unfold3D.exe file",
+        description = "Set the path to the Unfold3D Application",
         default = "",
         subtype = "DIR_PATH",
         get = get_unfold3DPath,
         set = set_unfold3DPath
     )
-    Optimize = bpy.props.IntProperty \
+    optimize = bpy.props.IntProperty \
     (
         name = "Interations",
         description = "Optimize UV Coordinate for less distortion (Number of iterations for the optimize algorithm)",
@@ -113,7 +133,45 @@ class B2Unfold_Settings(bpy.types.PropertyGroup):
         min = 1,
         max = 750,
     )
+    autoSeamAlgorithm = bpy.props.EnumProperty \
+    (
+        name = "Algorithm",
+        description = "This Auto Seams dropdown contains advanced algorithms to select edges automatically of the visible island set. These edges serve as a good candidate to cut and segment the islands",
+                 #(identifier, name, description, icon, number)
+        items = [ ('0', "Hierarchical Pelt Algorithm", ''),
+                  ('1', "Mosaic Algorithm",''),
+                  ('2', "Sharpest Angles Algorithm",''),
+                ],
+        default = '2',
+        update = set_algorithm
+    )
 
+    sharpestAngle = bpy.props.IntProperty \
+    (
+        name = "Sharpest Angle",
+        description = "Edges that have their polygon's normals forming an angle superior to this value will be selected",
+        default = 60,
+        min = 1,
+        max = 89,
+        update = jangle
+    )
+
+    mosaicForce = bpy.props.FloatProperty \
+    (
+        name = "Mosaic Force",
+        description = "High values will segment more so the islands will be unfolded with less distortion. Low values will segment less but the cut will generate more distortion",
+        default = 0.5,
+        min = 0.001,
+        max = 0.999,
+        update = jangle
+    )
+
+algorithmCMDStrings = \
+["U3dSelect({PrimType='Edge', Select=true, ResetBefore=true, WorkingSetPrimType='Island', ProtectMapName='Protect', FilterIslandVisible=true, Auto={Skeleton={}, Open=true, PipesCutter=true, HandleCutter=true}})", 
+"U3dSelect({PrimType='Edge', Select=true, ResetBefore=true, WorkingSetPrimType='Island', ProtectMapName='Protect', FilterIslandVisible=true, Auto={QuasiDevelopable={Developability=" + str(bpy.context.scene.B2Unfold_Settings.mosaicForce) + ", IslandPolyNBMin=5, FitCones=false, Straighten=true}, PipesCutter=true, HandleCutter=true}})", 
+"U3dSelect({PrimType='Edge', Select=true, ResetBefore=true, WorkingSetPrimType='Island', ProtectMapName='Protect', FilterIslandVisible=true, Auto={SharpEdges={AngleMin=" + str(bpy.context.scene.B2Unfold_Settings.sharpestAngle) + "}, PipesCutter=true, HandleCutter=true}})"
+]
+algorithmString = ""
 
 # ---------------------------------------- USER INTEFACE --------------------------------------------
 
@@ -125,6 +183,15 @@ class B2Unfold(bpy.types.Operator):
     def execute(self, context):
         B2Unfold_LinkFunction()
         return {'FINISHED'}
+
+class MosaicButton(bpy.types.Operator):
+    bl_idname = "mosaic.value"
+    bl_label = "force"
+    mosaicValue = bpy.props.FloatProperty()
+
+    def execute(self, context):
+        set_mosaicForce(self.mosaicValue)
+        return{'FINISHED'}
 
 class UnfoldUVMain(bpy.types.Panel):
     """Creates Main Panel in the Object properties window"""
@@ -138,21 +205,44 @@ class UnfoldUVMain(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
+        # ---------- SEND BUTTON ------------
         sendButton = layout.row()
         sendButton.scale_y = 2.5
         sendButton.operator(B2Unfold.bl_idname, text="Send To Unfold3D!", icon='TEXTURE_DATA')
 
+        # ---------- PATH INPUT -------------
         box = layout.box()
         box.label("Unfold3D Executable Path",icon = "FILTER")
         exePath = box.column(True)
         exePath.prop(bpy.context.scene.B2Unfold_Settings, 'unfold3DPath')
-        
+
+        # -------- SETTINGS WINDOW ----------
         settingsBox = layout.box()
-        settingsBox.label("Unfold3D Unwrapping Settings")
+        settingsBox.label("Unfold3D Settings")
+        algorithmOps = settingsBox.column(True)
+        algorithmOps.prop(bpy.context.scene.B2Unfold_Settings, "autoSeamAlgorithm", icon = "EDGESEL")
         
-        iterations = settingsBox.column(True)
+        if(bpy.context.scene.B2Unfold_Settings.autoSeamAlgorithm == '0'):
+            algorithmBox = settingsBox.box()
+            
+
+        elif(bpy.context.scene.B2Unfold_Settings.autoSeamAlgorithm == '1'):
+            algorithmBox = settingsBox.box()
+            forceButtons = algorithmBox.row(align=True)
+            forceButtons.operator(MosaicButton.bl_idname, text="1").mosaicValue = 0.25
+            forceButtons.operator(MosaicButton.bl_idname, text="2").mosaicValue = 0.5
+            forceButtons.operator(MosaicButton.bl_idname, text="3").mosaicValue = 0.75
+            forceButtons.operator(MosaicButton.bl_idname, text="4").mosaicValue = 0.95
+            algorithmBox.prop(bpy.context.scene.B2Unfold_Settings, "mosaicForce")
+            
+        elif(bpy.context.scene.B2Unfold_Settings.autoSeamAlgorithm == '2'):
+            algorithmBox = settingsBox.box()
+            algorithmBox.prop(bpy.context.scene.B2Unfold_Settings, "sharpestAngle")
+        
+        iterations = settingsBox.box()
         iterations.label("Optimize")
-        iterations.prop(bpy.context.scene.B2Unfold_Settings, "Optimize")
+        iterations.prop(bpy.context.scene.B2Unfold_Settings, "optimize")
+            
 
 def register():
     bpy.utils.register_module(__name__)
